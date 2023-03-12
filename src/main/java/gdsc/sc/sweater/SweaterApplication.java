ea7926872a2359d8f7c8f4ecfd5b4d25a14e2da8
KIM TAEHYUN <tanggu1221@gmail.com>
2023-03-13 00:01:05 +0900

    initial commit

---
+++ src/main/java/gdsc/sc/sweater/SweaterApplication.java
@@ -0,0 +1,13 @@
+package gdsc.sc.sweater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gdsc/sc/sweater/SweaterApplicationTests.java
@@ -0,0 +1,13 @@
+package gdsc.sc.sweater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
